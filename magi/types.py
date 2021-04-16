3b4fad42f5866a5e8b123c04a77623ae46564373
Luo Yicheng <ethanluoyc@gmail.com>
2021-04-16 11:26:04 +0100

    Add core interfaces. (#1)
    This adds interfaces that the agent in Magi should conform to.
    The interfaces are adapted from Acme, but we copied the
    interfaces here so that in the future we can customize it
    for our needs.

---
+++ magi/types.py
@@ -0,0 +1,14 @@
+"""Common types used throughout Magi."""
+
+from typing import Any, Iterable, Mapping, Union
+
+from dm_env import specs
+
+NestedArray = Any
+NestedTensor = Any
+
+NestedSpec = Union[
+    specs.Array,
+    Iterable['NestedSpec'],
+    Mapping[Any, 'NestedSpec'],  # pytype: disable=not-supported-yet
+]
