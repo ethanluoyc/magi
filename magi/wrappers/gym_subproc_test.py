d44d1442afe0b3636f8b8d07336d6e307c4dc934
Yicheng Luo <ethanluoyc@gmail.com>
2021-05-16 18:09:47 +0100

    Add Gym SubprocEnv.

---
+++ magi/wrappers/gym_subproc_test.py
@@ -0,0 +1,22 @@
+"""Test for subprocess wrapper."""
+from absl.testing import absltest
+import gym
+
+from magi.wrappers import gym_subproc
+
+
+class SubProcessWrapperTest(absltest.TestCase):
+
+  def test_subproc_env(self):
+    """Test that subproc env runs"""
+    env = gym_subproc.SubprocEnv(lambda **kwargs: gym.make("CartPole-v1"))
+    env.seed()
+    env.reset()
+    while True:
+      _, _, done, _ = env.step(env.action_space.sample())
+      if done:
+        break
+
+
+if __name__ == "__main__":
+  absltest.main()
