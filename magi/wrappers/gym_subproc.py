b3744c629e9f458ca7bc3e6bf786ec200366c21a
Yicheng Luo <ethanluoyc@gmail.com>
2021-06-19 20:01:31 +0100

    Update README. (#26)
    * Update README.
    * Include license information for some adapted code

--- magi/wrappers/gym_subproc.py
+++ magi/wrappers/gym_subproc.py
@@ -1,6 +1,8 @@
 """Wrapper for launching environment in a subprocess.
 Code adapted from https://github.com/openai/gym3/blob/master/gym3/subproc.py
 """
+# MIT License
+# Copyright (c) 2020 OpenAI
 import contextlib
 import multiprocessing as mp
 import os
