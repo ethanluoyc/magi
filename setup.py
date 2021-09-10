4da9d56e060780a6527293f2b35db6a6d54874ed
Yicheng Luo <ethanluoyc@gmail.com>
2021-09-10 16:15:46 +0100

    Update requirements (#45)
    - Update to use dm-acme==0.2.0.
    - Remove requirements/ and move extra dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-
 import setuptools
-
-
-def parse_requirements_file(path):
-    return [line.rstrip() for line in open(path, "r") if not line.startswith("#")]
-
-
-main_requirements = parse_requirements_file("requirements/main.txt")
-dev_requirements = parse_requirements_file("requirements/dev.txt")
-envs_requirements = parse_requirements_file("requirements/envs.txt")
 
 if __name__ == "__main__":
     setuptools.setup(
-        install_requires=main_requirements,
-        extras_require={"dev": dev_requirements, "envs": envs_requirements},
+        install_requires=[
+            "jax",
+            "jaxlib",
+            "dm-acme[jax]==0.2.2",
+            "wandb",
+            "ml_collections",
+        ],
+        extras_require={"envs": ["gym", "bsuite", "dm-control"]},
     )
