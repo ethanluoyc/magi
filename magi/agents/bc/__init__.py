2d367367143ae8df59c872e7962e953e3a8c4ede
Yicheng Luo <ethanluoyc@gmail.com>
2021-09-20 15:26:52 +0100

    Add BC agent

---
+++ magi/agents/bc/__init__.py
@@ -0,0 +1,5 @@
+from acme.agents.jax.bc.losses import logp  # noqa
+from acme.agents.jax.bc.losses import mse  # noqa
+from acme.agents.jax.bc.losses import peerbc  # noqa
+
+from magi.agents.bc.learning import BCLearner  # noqa
