274e5b2df40a5403558d8dc3d55a996021aa5330
Yicheng Luo <ethanluoyc@gmail.com>
2021-10-08 15:56:16 +0100

    Fix and simplify CRR
    - Remove distribution and custom categorical loss.
    - Fix a bug of not using the discount.

--- magi/agents/crr/learning.py
+++ magi/agents/crr/learning.py
@@ -5,17 +5,15 @@ from typing import Dict, Iterator, List, NamedTuple, Optional, Sequence, Tuple
 import acme
 from acme import types
 from acme.jax import networks as networks_lib
-from acme.jax import utils as jax_utils
 from acme.utils import counting
 from acme.utils import loggers
+import chex
 import haiku as hk
 import jax
 import jax.numpy as jnp
 import optax
 import reverb
-
-from magi.agents.crr import distributions
-from magi.agents.crr import losses
+import rlax
 
 
 class TrainingState(NamedTuple):
@@ -51,16 +49,6 @@ class CRRLearner(acme.Learner):
         counter: Optional[counting.Counter] = None,
         logger: Optional[loggers.Logger] = None,
     ):
-        self._policy_optimizer = (
-            policy_optimizer
-            if policy_optimizer
-            else optax.chain(optax.clip_by_global_norm(40.0), optax.adam(1e-4))
-        )
-        self._critic_optimizer = (
-            critic_optimizer
-            if critic_optimizer
-            else optax.chain(optax.clip_by_global_norm(40.0), optax.adam(1e-4))
-        )
         if policy_improvement_modes not in {"exp", "binary", "all"}:
             raise ValueError(
                 "policy_improvement_modes must be one of ['exp', 'binary', 'all'], "
@@ -71,37 +59,19 @@ class CRRLearner(acme.Learner):
                 "baseline_reduce_function must be one of ['mean', 'max', 'min'], "
                 f"got {baseline_reduce_function}"
             )
-        # Internalize agent parameters
-        self._policy_network = policy_network
-        self._critic_network = critic_network
-        self._data_iterator = dataset
-        self._discount = discount
-        self._target_update_period = target_update_period
-        self._beta = beta
-        self._num_action_samples_td_learning = num_action_samples_td_learning
-        self._num_action_samples_policy_weight = num_action_samples_policy_weight
-        self._baseline_reduce_function = baseline_reduce_function
-        self._policy_improvement_modes = policy_improvement_modes
-        self._ratio_upper_bound = ratio_upper_bound
-        self._logger = logger or loggers.make_default_logger("learner", save_data=False)
-        self._counter = counter or counting.Counter()
-
-        # Initialize state
-        policy_init_key, critic_init_key, random_key = jax.random.split(random_key, 3)
-        initial_policy_params = policy_network.init(policy_init_key)
-        initial_policy_opt_state = self._policy_optimizer.init(initial_policy_params)
-        initial_critic_params = critic_network.init(critic_init_key)
-        initial_critic_opt_state = self._critic_optimizer.init(initial_critic_params)
-        self._state = TrainingState(
-            steps=0,
-            random_key=random_key,
-            policy_params=initial_policy_params,
-            critic_params=initial_critic_params,
-            policy_opt_state=initial_policy_opt_state,
-            critic_opt_state=initial_critic_opt_state,
-            policy_target_params=copy.deepcopy(initial_policy_params),
-            critic_target_params=copy.deepcopy(initial_critic_params),
+        policy_optimizer = policy_optimizer or optax.chain(
+            optax.clip_by_global_norm(40.0), optax.adam(1e-4)
         )
+        critic_optimizer = critic_optimizer or optax.chain(
+            optax.clip_by_global_norm(40.0), optax.adam(1e-4)
+        )
+
+        def critic_mean(logits, atoms):
+            """Compute the expected value from distributional critic"""
+            chex.assert_rank(logits, 2)
+            chex.assert_rank(atoms, 1)
+            probabilities = jax.nn.softmax(logits)
+            return jnp.sum(probabilities * atoms, axis=-1)
 
         def loss_fn(
             online_params: Tuple[networks_lib.Params, networks_lib.Params],
@@ -117,73 +87,61 @@ class CRRLearner(acme.Learner):
             o_tm1 = transitions.observation
             a_tm1 = transitions.action
             r_t = transitions.reward
-            d_t = transitions.discount
+            d_t = discount * transitions.discount
             o_t = transitions.next_observation
             # ========================= Critic learning =============================
-            q_tm1 = self._critic_network.apply(critic_params, o_tm1, a_tm1)
-            q_tm1 = distributions.DiscreteValuedDistribution(
-                values=q_tm1.values, logits=q_tm1.logits
+            q_tm1_logits, q_tm1_atoms = critic_network.apply(
+                critic_params, o_tm1, a_tm1
             )
-            target_action_distribution = self._policy_network.apply(
-                policy_target_params, o_t
-            )
+            target_action_distribution = policy_network.apply(policy_target_params, o_t)
             sampled_actions_t = target_action_distribution.sample(
-                self._num_action_samples_td_learning, seed=a_t_key
+                num_action_samples_td_learning, seed=a_t_key
             )
             # [N, B, ...]
-            tiled_o_t = jax_utils.tile_nested(o_t, self._num_action_samples_td_learning)
             # Compute the target critic's Q-value of the sampled actions.
-            sampled_q_t = jax.vmap(self._critic_network.apply, (None, 0, 0))(
-                critic_target_params, tiled_o_t, sampled_actions_t
-            )
-            # Compute average logits by first reshaping them to [N, B, A] and then
-            # normalizing them across atoms.
-            new_shape = [self._num_action_samples_td_learning, r_t.shape[0], -1]
-            sampled_logits = jnp.reshape(sampled_q_t.logits, new_shape)
-            sampled_logprobs = jax.nn.log_softmax(sampled_logits, axis=-1)
-            averaged_logits = jax.scipy.special.logsumexp(sampled_logprobs, axis=0)
+            sampled_q_t_logits, sampled_q_t_atoms = jax.vmap(
+                critic_network.apply, (None, None, 0)
+            )(critic_target_params, o_t, sampled_actions_t)
 
-            # Construct the expected distributional value for bootstrapping.
-            # We take sampled_q_t.values[0] here as the vmap returns a batch
-            # of same values while losses.categorical expects 1D support
-            q_t = distributions.DiscreteValuedDistribution(
-                values=sampled_q_t.values[0], logits=averaged_logits
+            sampled_logits = sampled_q_t_logits
+            sampled_logprobs = jax.nn.log_softmax(sampled_logits, axis=-1)
+            q_t_logits = jax.scipy.special.logsumexp(sampled_logprobs, axis=0)
+            # We take sampled_q_t_atoms[0] here as the vmap returns a batch
+            # of same values
+            q_t_atoms = sampled_q_t_atoms[0]
+
+            batch_td_learning = jax.vmap(
+                rlax.categorical_td_learning, in_axes=(None, 0, 0, 0, None, 0)
             )
-            critic_loss_t = losses.categorical(q_tm1, r_t, discount * d_t, q_t)
-            assert len(critic_loss_t.shape) == 1
+            # Construct the expected distributional value for bootstrapping.
+            critic_loss_t = batch_td_learning(
+                q_tm1_atoms, q_tm1_logits, r_t, d_t, q_t_atoms, q_t_logits
+            )
+            chex.assert_rank(critic_loss_t, 1)
             critic_loss_t = jnp.mean(critic_loss_t)
 
             # ========================= Actor learning =============================
-            action_distribution_tm1 = self._policy_network.apply(policy_params, o_tm1)
-            q_tm1_mean = q_tm1.mean()
+            action_distribution_tm1 = policy_network.apply(policy_params, o_tm1)
+            # Compute expected q_tm1
+            q_tm1_mean = critic_mean(q_tm1_logits, q_tm1_atoms)
 
             # Compute the estimate of the value function based on
             # self._num_action_samples_policy_weight samples from the policy.
-            tiled_o_tm1 = jax_utils.tile_nested(
-                o_tm1, self._num_action_samples_policy_weight
-            )
             action_tm1 = action_distribution_tm1.sample(
-                self._num_action_samples_policy_weight, seed=a_tm1_key
+                num_action_samples_policy_weight, seed=a_tm1_key
             )
             tiled_z_tm1_logits, tiled_z_tm1_values = jax.vmap(
-                self._critic_network.apply, (None, 0, 0)
-            )(critic_params, tiled_o_tm1, action_tm1)
-            tiled_z_tm1 = distributions.DiscreteValuedDistribution(
-                values=tiled_z_tm1_values[0],
-                logits=tiled_z_tm1_logits,
-            )
-            tiled_v_tm1 = jnp.reshape(
-                tiled_z_tm1.mean(),
-                [self._num_action_samples_policy_weight, -1],
-            )
+                critic_network.apply, (None, None, 0)
+            )(critic_params, o_tm1, action_tm1)
+            tiled_v_tm1 = jax.vmap(critic_mean)(tiled_z_tm1_logits, tiled_z_tm1_values)
 
             # Use mean, min, or max to aggregate Q(s, a_i), a_i ~ pi(s) into the
             # final estimate of the value function.
-            if self._baseline_reduce_function == "mean":
+            if baseline_reduce_function == "mean":
                 v_tm1_estimate = jnp.mean(tiled_v_tm1, axis=0)
-            elif self._baseline_reduce_function == "max":
+            elif baseline_reduce_function == "max":
                 v_tm1_estimate = jnp.max(tiled_v_tm1, axis=0)
-            elif self._baseline_reduce_function == "min":
+            elif baseline_reduce_function == "min":
                 v_tm1_estimate = jnp.min(tiled_v_tm1, axis=0)
 
             # Assert that action_distribution_tm1 is a batch of multivariate
@@ -193,19 +151,19 @@ class CRRLearner(acme.Learner):
             policy_loss_batch = -action_distribution_tm1.log_prob(a_tm1)
 
             advantage = q_tm1_mean - v_tm1_estimate
-            if self._policy_improvement_modes == "exp":
+            if policy_improvement_modes == "exp":
                 policy_loss_coef_t = jnp.minimum(
-                    jnp.exp(advantage / self._beta), self._ratio_upper_bound
+                    jnp.exp(advantage / beta), ratio_upper_bound
                 )
-            elif self._policy_improvement_modes == "binary":
+            elif policy_improvement_modes == "binary":
                 policy_loss_coef_t = (advantage > 0).astype(r_t.dtype)
-            elif self._policy_improvement_modes == "all":
+            elif policy_improvement_modes == "all":
                 # Regress against all actions (effectively pure BC).
                 policy_loss_coef_t = 1.0
             policy_loss_coef_t = jax.lax.stop_gradient(policy_loss_coef_t)
 
             policy_loss_batch *= policy_loss_coef_t
-            assert len(policy_loss_batch.shape) == 1
+            chex.assert_rank(policy_loss_batch, 1)
             policy_loss_t = jnp.mean(policy_loss_batch)
 
             policy_loss_coef = jnp.mean(policy_loss_coef_t)  # For logging.
@@ -238,10 +196,10 @@ class CRRLearner(acme.Learner):
                     "critic_g_norm": optax.global_norm(critic_grads),
                 }
             )
-            policy_updates, new_policy_opt_state = self._policy_optimizer.update(
+            policy_updates, new_policy_opt_state = policy_optimizer.update(
                 policy_grads, state.policy_opt_state
             )
-            critic_updates, new_critic_opt_state = self._critic_optimizer.update(
+            critic_updates, new_critic_opt_state = critic_optimizer.update(
                 critic_grads, state.critic_opt_state
             )
             new_critic_params = optax.apply_updates(state.critic_params, critic_updates)
@@ -250,7 +208,7 @@ class CRRLearner(acme.Learner):
                 (new_policy_params, new_critic_params),
                 target_params,
                 steps=state.steps,
-                update_period=self._target_update_period,
+                update_period=target_update_period,
             )
             new_state = state._replace(
                 steps=state.steps + 1,
@@ -264,7 +222,30 @@ class CRRLearner(acme.Learner):
             )
             return new_state, metrics
 
+        def make_initial_state(key):
+            policy_init_key, critic_init_key, random_key = jax.random.split(key, 3)
+            initial_policy_params = policy_network.init(policy_init_key)
+            initial_policy_opt_state = policy_optimizer.init(initial_policy_params)
+            initial_critic_params = critic_network.init(critic_init_key)
+            initial_critic_opt_state = critic_optimizer.init(initial_critic_params)
+            return TrainingState(
+                steps=0,
+                random_key=random_key,
+                policy_params=initial_policy_params,
+                critic_params=initial_critic_params,
+                policy_opt_state=initial_policy_opt_state,
+                critic_opt_state=initial_critic_opt_state,
+                policy_target_params=copy.deepcopy(initial_policy_params),
+                critic_target_params=copy.deepcopy(initial_critic_params),
+            )
+
         self._sgd_step = jax.jit(sgd_step)
+        self._data_iterator = dataset
+        # Initialize state
+        self._state = make_initial_state(random_key)
+        # Set up logger and counter
+        self._logger = logger or loggers.make_default_logger("learner", save_data=False)
+        self._counter = counter or counting.Counter()
 
     def step(self):
         # Sample replay buffer
--- magi/agents/crr/networks.py
+++ magi/agents/crr/networks.py
@@ -5,7 +5,6 @@ from acme.agents.jax import actors
 from acme.jax import networks as networks_lib
 from acme.jax import utils
 import haiku as hk
-import jax
 import jax.numpy as jnp
 import numpy as np
 import tensorflow_probability.substrates.jax as tfp
@@ -15,7 +14,7 @@ tfd = tfp.distributions
 
 class DiscreteValuedHeadOutput(NamedTuple):
     logits: jnp.ndarray
-    values: jnp.ndarray
+    atoms: jnp.ndarray
 
 
 class DiscreteValuedHead(hk.Module):
@@ -43,9 +42,9 @@ class DiscreteValuedHead(hk.Module):
           b_init: Initialization for linear layer biases.
         """
         super().__init__(name="DiscreteValuedHead")
-        self._values = jnp.linspace(vmin, vmax, num_atoms, axis=-1)
+        self._atoms = jnp.linspace(vmin, vmax, num_atoms, axis=-1)
         self._distributional_layer = hk.Linear(
-            self._values.shape[0], w_init=w_init, b_init=b_init
+            self._atoms.shape[0], w_init=w_init, b_init=b_init
         )
 
     def __call__(self, inputs: jnp.ndarray) -> tfd.Distribution:
@@ -53,77 +52,11 @@ class DiscreteValuedHead(hk.Module):
         logits = jnp.reshape(
             logits,
             # Use numpy here since we are computing the shapes statically
-            np.concatenate(
-                [logits.shape[:1], self._values.shape], axis=0  # batch size
-            ),
+            np.concatenate([logits.shape[:1], self._atoms.shape], axis=0),  # batch size
         )
-        values = self._values.astype(logits.dtype)
+        atoms = self._atoms.astype(logits.dtype)
 
-        return DiscreteValuedHeadOutput(logits=logits, values=values)
-
-
-class MultivariateNormalDiagHead(hk.Module):
-    """Module that produces a multivariate normal distribution using tfd.Independent or
-    tfd.MultivariateNormalDiag.
-    """
-
-    def __init__(
-        self,
-        num_dimensions: int,
-        init_scale: float = 0.3,
-        min_scale: float = 1e-6,
-        tanh_mean: bool = False,
-        fixed_scale: bool = False,
-        use_tfd_independent: bool = False,
-        w_init: hk.initializers.Initializer = hk.initializers.VarianceScaling(1e-4),
-        b_init: hk.initializers.Initializer = hk.initializers.Constant(0),
-    ):
-        """Initialization.
-
-        Args:
-          num_dimensions: Number of dimensions of MVN distribution.
-          init_scale: Initial standard deviation.
-          min_scale: Minimum standard deviation.
-          tanh_mean: Whether to transform the mean (via tanh) before passing it to
-            the distribution.
-          fixed_scale: Whether to use a fixed variance.
-          use_tfd_independent: Whether to use tfd.Independent or
-            tfd.MultivariateNormalDiag class
-          w_init: Initialization for linear layer weights.
-          b_init: Initialization for linear layer biases.
-        """
-        super().__init__(name="MultivariateNormalDiagHead")
-        self._init_scale = init_scale
-        self._min_scale = min_scale
-        self._tanh_mean = tanh_mean
-        self._mean_layer = hk.Linear(num_dimensions, w_init=w_init, b_init=b_init)
-        self._fixed_scale = fixed_scale
-
-        if not fixed_scale:
-            self._scale_layer = hk.Linear(num_dimensions, w_init=w_init, b_init=b_init)
-        self._use_tfd_independent = use_tfd_independent
-
-    def __call__(self, inputs: jnp.ndarray) -> tfd.Distribution:
-        zero = 0
-        mean = self._mean_layer(inputs)
-
-        if self._fixed_scale:
-            scale = jnp.ones_like(mean) * self._init_scale
-        else:
-            scale = jax.nn.softplus(self._scale_layer(inputs))
-            scale *= self._init_scale / jax.nn.softplus(zero)
-            scale += self._min_scale
-
-        # Maybe transform the mean.
-        if self._tanh_mean:
-            mean = jnp.tanh(mean)
-
-        if self._use_tfd_independent:
-            dist = tfd.Independent(tfd.Normal(loc=mean, scale=scale))
-        else:
-            dist = tfd.MultivariateNormalDiag(loc=mean, scale_diag=scale)
-
-        return dist
+        return DiscreteValuedHeadOutput(logits=logits, atoms=atoms)
 
 
 def apply_policy_and_sample(
