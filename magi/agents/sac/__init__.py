6f2aa93500aa1506ca45ab94a9e474dec169ae25
Yicheng Luo <ethanluoyc@gmail.com>
2021-10-11 16:30:14 +0100

    Expose SAC builder

--- magi/agents/sac/__init__.py
+++ magi/agents/sac/__init__.py
@@ -1,4 +1,5 @@
 from magi.agents.sac.agent import SACAgent  # noqa
+from magi.agents.sac.builder import SACBuilder  # noqa
 from magi.agents.sac.config import SACConfig  # noqa
 from magi.agents.sac.config import target_entropy_from_env_spec  # noqa
 from magi.agents.sac.learning import SACLearner  # noqa
