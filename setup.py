6d900a8feffaf079f97da1906067ca8c0863865e
Yicheng Luo <ethanluoyc@gmail.com>
2021-11-04 00:36:32 +0000

    Some minor updates to requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
+"""setuptools shim"""
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(
-        install_requires=[
-            "jax",
-            "jaxlib",
-            "dm-acme[jax]==0.2.2",
-            "wandb",
-            "ml_collections",
-        ],
-        extras_require={"envs": ["gym", "bsuite", "dm-control"]},
-    )
+    setuptools.setup()
