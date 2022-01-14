1cc3fea384238559e2f1d445d7267ec2820a74ab
Yicheng Luo <ethanluoyc@gmail.com>
2022-01-14 16:26:04 +0000

    feat(mpo): Add MPO agent implementation
    Add a JAX MPO agent implementation based on acme/agents/tf/mpo.
    
    Some notes on the implementation
    
    1. It uses a single pass for computing the gradients wrt
       policy and critic using vjp. This is similar to the way
       a single pass is used for computing gradients in the jax
       value_dice agent and is the closest to what is being done
       in the tf mpo agent.
    2. It uses a single network for the policy network and does not
       use a distributional critic so it's not dmpo. It's also
       at the moment incompatible with the CRR agent where we use
       a double critic architecture. We can probably
       implement a generalized version of the mpo agent which allows
       users to choose between different critics.
    3. The networks match what's being used in dmpo. Most importantly,
       by default, it's a large network using layernorm and mvn diag head
       that does not squash the mvn. Out of bounds actions is penalized
       in the mpo policy loss.
    4. The mpo loss depends on dm-acme>=0.4.0
    
    Recent version of Acme moves mpo params clipping out. See
    https://github.com/deepmind/acme/commit/d436d6be4744ca7ad00f039a4781a445fb84f3c4

---
+++ magi/agents/mpo/__init__.py
@@ -0,0 +1,6 @@
+"""MPO agent implementation"""
+from magi.agents.mpo.agent import MPO
+from magi.agents.mpo.builder import MPOBuilder
+from magi.agents.mpo.config import MPOConfig
+from magi.agents.mpo.networks import apply_policy_and_sample
+from magi.agents.mpo.networks import make_networks
---
+++ magi/agents/mpo/config.py
@@ -0,0 +1,27 @@
+"""Configuration for MPO agent"""
+import dataclasses
+from typing import Optional
+
+from acme.adders import reverb as adders_reverb
+
+
+@dataclasses.dataclass
+class MPOConfig:
+    """MPO agent parameters."""
+
+    min_replay_size: int = 1000
+    max_replay_size: int = 1_000_000
+    replay_table_name: str = adders_reverb.DEFAULT_PRIORITY_TABLE
+    prefetch_size: Optional[int] = None
+    samples_per_insert: float = 256
+
+    discount: float = 0.99
+    batch_size: int = 256
+    num_samples: int = 20
+    n_step: int = 5
+    clipping: bool = True
+    policy_learning_rate = 1e-4
+    critic_learning_rate = 1e-4
+    dual_learning_rate = 1e-2
+    target_policy_update_period: int = 100
+    target_critic_update_period: int = 100
